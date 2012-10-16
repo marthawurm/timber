40654f7e392d9643712644fe6553e6a585c85d54
NjeriChelimo <martha.c.chumo@gmail.com>
2012-10-16 04:50:39 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timber::Application.config.secret_token = '991079067945e92ed2f91898af5045b88bc236d7a99c5e149051f84b6bac89bdab3cfded5f555696227d912cf509ffe21e366e930c9d530f2cbb55536771c68e'
